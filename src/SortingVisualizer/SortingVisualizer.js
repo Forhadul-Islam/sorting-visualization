41227abebb9871f2983c6904c0e0012dfeb1bd2c
Mizanur Rahaman <forhadulislam2100@gmial.com>
2020-11-05 02:21:12 +0600

    some style implimented

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -2,21 +2,17 @@ import React, { useEffect, useState } from 'react'
 import './SortingVisualizer.css'
 import {getMergeSortAnimations} from '../SortingAlgorithms/SortingAlgorithms';
 
-// Change this value for the speed of the animations.
+
+
 const ANIMATION_SPEED_MS = 1;
-
-// Change this value for the number of bars (value) in the array.
 const NUMBER_OF_ARRAY_BARS = 310;
-
-// This is the main color of the array bars.
 const PRIMARY_COLOR = 'turquoise';
-
-// This is the color of array bars that are being compared throughout the animations.
 const SECONDARY_COLOR = 'red';
 
 const SortingVisualizer = () => {
     const [numbers, setNumbers] = useState([])
     const [isSorting, setIsSorting] =useState(false)
+    const [primaryColor, setPrimaryColor] = useState('#6eb49a')
     
     function getRandomArbitrary(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -43,11 +39,11 @@ const SortingVisualizer = () => {
         const [barOneIdx, barTwoIdx] = animations[i];
         const barOneStyle = arrayBars[barOneIdx].style;
         const barTwoStyle = arrayBars[barTwoIdx].style;
-        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-        // setTimeout(() => {
-        //   barOneStyle.backgroundColor = color;
-        //   barTwoStyle.backgroundColor = color;
-        // }, i * ANIMATION_SPEED_MS);
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR ;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
       } else {
         setTimeout(() => {
           const [barOneIdx, newHeight] = animations[i];
@@ -72,12 +68,19 @@ const SortingVisualizer = () => {
             <div className="array-container">
             {
                 numbers.map((value, index) =>{
-                   return <div style={{height:`${value}px`}} className="array-bar"></div>
+                   return <div style={{height:`${value}px` , backgroundColor:primaryColor}} className="array-bar"></div>
                 })
             }
             </div>
             
             <div className="sortingVisualizer__controller">
+                <div className="color-section">
+                    <p>Color: </p>
+                    <div style={{backgroundColor: "aqua"}} onClick={()=>setPrimaryColor('aqua')} className="colorOption"></div>
+                    <div style={{backgroundColor: "#7f3cc9"}} onClick={()=>setPrimaryColor('#7f3cc9')} className="colorOption"></div>
+                    <div style={{backgroundColor: "#b4a578"}} onClick={()=>setPrimaryColor('#b4a578')} className="colorOption"></div>
+                    <div style={{backgroundColor: "#6eb49a"}} onClick={()=>setPrimaryColor('#6eb49a')} className="colorOption"></div>
+                </div>
                 <button className="barGenerateButton" disabled={isSorting} onClick={() => resetArray()}>Generate New Array</button>
                 <button className="mainButton" disabled={isSorting} onClick={() => mergeSort()}>Merge Sort</button>
                 <button className="mainButton" disabled={isSorting} onClick={() => quickSort()}>Quick Sort</button>
